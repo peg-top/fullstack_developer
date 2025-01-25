e0ebfb6cc3069de276c1ce5ce868b6f86aeb1407
peg-top <94269518+peg-top@users.noreply.github.com>
2025-01-25 13:52:32 -0800

    step 2 refactoring

--- part-1/1_1_Course_Information/src/App.jsx
+++ part-1/1_1_Course_Information/src/App.jsx
@@ -13,46 +13,49 @@ const CourseInfo = ({ course, exercises }) => {
   ) 
 }
 
-const Total = ( { courses }) => (
+const Total = ({ courses }) => (
     <p>Number of exercises {courses.reduce((sum, {exercises}) => sum + exercises, 0)}</p>
   ) 
 
+
+const Content = ({ courseData }) => {
+  const { title, courses } = courseData
+  return (
+    <>
+      <Header title={title} />
+      {courses.map((course) => (
+        <CourseInfo key={course.id} course={course.name} exercises={course.exercises} />
+      ))}
+      <Total courses={courses} />
+    </>
+  )}
+
 const App = () => {
 
-  const coursesTitle = 'Half Stack application development'
-  const courses = [
-    {
-      id: 0,
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      id: 1,
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      id : 2,
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-  const Content = ({ courses }) => {
-    return (
-      <>
-        {courses.map((course) => (
-          <CourseInfo key={course.id} course={course.name} exercises={course.exercises} />
-        ))}
-      </>
-    )}
-  
+  const courseData = {
+    title: 'Half Stack application development',
+    courses: [
+      {
+        id: 0,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 1,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id : 2,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header title={coursesTitle} />
-      <Content courses={courses} />
-      <Total courses={courses} />
+      <Content courseData={courseData} />
     </>
   )
 }
