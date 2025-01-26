980ea4af813c5723e81dfbb5adc8465cc6f837f6
peg-top <94269518+peg-top@users.noreply.github.com>
2025-01-26 06:30:16 -0800

    1.6 step 1

--- part-1/16_14_Exercises/src/App.jsx
+++ part-1/16_14_Exercises/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () =>{
 
   console.log('App component')
-  
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <>
-      <h1>Exercises</h1>
+      <main>
+        <section>
+          <h1>Give Feedback</h1>
+          <button onClick={() => setGood(good + 1)}>good</button>
+          <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+          <button onClick={() => setBad(bad + 1)}>bad</button>
+        </section>
+        <section>
+          <h1>Statistics</h1>
+          <ul>
+            <li>good {good}</li>
+            <li>neutral {neutral}</li>
+            <li>bad {bad}</li>
+          </ul>
+        </section>
+      </main>
+
     </>
   )
 }
