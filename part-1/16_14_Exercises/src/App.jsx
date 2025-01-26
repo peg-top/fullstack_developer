0fa579904c3db4a4ad81c7f42213f12d6216c890
peg-top <94269518+peg-top@users.noreply.github.com>
2025-01-26 06:57:03 -0800

    1.6 step 3

--- part-1/16_14_Exercises/src/App.jsx
+++ part-1/16_14_Exercises/src/App.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
 import { useState } from 'react'
 
-const App = () =>{
+
+const Feedback = ({ buttons }) => {
+  console.log('Feedback component')
+  return (
+    <section>
+      <h1>Give feedback</h1>
+      {buttons.map(({ id, text, handleClick }) => (
+        <Btn key={id} text={text} handleClick={handleClick} />
+      ))}
+    </section>
+  )
+}
+
+const Btn = ({ text, handleClick }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  console.log('Statistics component')
+  return (  
+    <section>
+      <h1>Statistics</h1>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+        <li>all {good + neutral + bad}</li>
+        <li>average {(good + neutral + bad)/3}</li>
+        <li>positive {good / (good + neutral + bad) * 100} %</li>
+      </ul>
+    </section>
+  )
+}
+
+
+const App = () => {
 
   console.log('App component')
 
@@ -9,29 +46,30 @@ const App = () =>{
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <>
-      <main>
-        <section>
-          <h1>Give Feedback</h1>
-          <button onClick={() => setGood(good + 1)}>good</button>
-          <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-          <button onClick={() => setBad(bad + 1)}>bad</button>
-        </section>
-        <section>
-          <h1>Statistics</h1>
-          <ul>
-            <li>good {good}</li>
-            <li>neutral {neutral}</li>
-            <li>bad {bad}</li>
-            <li>all {good + neutral + bad}</li>
-            <li>average {(good + neutral + bad)/3}</li>
-            <li>positive {good / (good + neutral + bad) * 100} %</li>
-          </ul>
-        </section>
-      </main>
+  const buttons = [
+      {
+        id: 1,
+        text: 'good',
+        handleClick () { setGood(good + 1) }
+      }
+      ,
+      {
+        id: 2,
+        text: 'neutral',
+        handleClick() { setNeutral(neutral + 1) }
+      },
+      {
+        id: 3,
+        text: 'bad',
+        handleClick() {setBad(bad + 1)}
+      }
+    ]
 
-    </>
+  return (
+    <main>
+      <Feedback buttons={buttons} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </main>
   )
 }
 
