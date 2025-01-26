1f162a496103f1670cd3a36482d1e16159d74515
peg-top <94269518+peg-top@users.noreply.github.com>
2025-01-26 07:47:35 -0800

    1.12-anecdotes step 3

--- part-1/1.12-anecdotes/src/App.jsx
+++ part-1/1.12-anecdotes/src/App.jsx
@@ -1,28 +1,51 @@
 import React from 'react'
 import { useState } from 'react'
 
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  'The only way to go fast, is to go well.'
+]
+
+
 const App = () => {
 
   console.log('App component')
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length))
-  const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
 
-  
+  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length))
+  const [votes, setVotes] = useState({})
+
+  const handleNextAnectote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const handleVote = () => {
+
+    console.log('handleVote:', selected)
+    console.log('votes:', votes)
+
+    if (votes[selected]){
+      setVotes({
+        ...votes,
+        [selected]: votes[selected] + 1
+      })
+    } else {
+      setVotes({
+        ...votes,
+        [selected]: 1
+      })
+    }
+  }
   
   return (
     <>
       <p>{anecdotes[selected]}</p>
-      <button onClick={handleClick}>next anecdotes</button>
+      <p>has {votes[selected] || 0} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNextAnectote}>next anecdotes</button>
     </>
   )
 }
