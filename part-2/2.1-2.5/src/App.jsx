ff6d46689cfbae0ad5e68d507d28327bb7735dc5
peg-top <94269518+peg-top@users.noreply.github.com>
2025-01-26 08:47:16 -0800

    2.1 step 9

--- part-2/2.1-2.5/src/App.jsx
+++ part-2/2.1-2.5/src/App.jsx
@@ -1,32 +1,79 @@
 import React from 'react'
 import Course from './Course'
 
-const courseData = {
-  title: 'Half Stack application development',
-  courses: [
-    {
-      id: 0,
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      id: 1,
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      id : 2,
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+// const courseData = {
+//   title: 'Half Stack application development',
+//   courses: [
+//     {
+//       id: 0,
+//       name: 'Fundamentals of React',
+//       exercises: 10
+//     },
+//     {
+//       id: 1,
+//       name: 'Using props to pass data',
+//       exercises: 7
+//     },
+//     {
+//       id : 2,
+//       name: 'State of a component',
+//       exercises: 14
+//     }
+//   ]
+// }
+
+const courses = [
+  {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }, 
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }
+]
 
 
 const App = () => {
   return (
     <>
-      <Course courseData={courseData} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </>
   )
 }
--- part-2/2.1-2.5/src/Course.jsx
+++ part-2/2.1-2.5/src/Course.jsx
@@ -3,16 +3,16 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({ courseData }) => {
+const Course = ({ course }) => {
 
-    const { title, courses } = courseData 
-    console.log("Component Course: ", title, courses)
+    const { name, parts } = course 
+    console.log("Component Course: ", name, parts)
   
     return (
       <>
-        <Header title={title} />
-        <Content courses={courses} />
-        <Total courses={courses} />
+        <Header title={name} />
+        <Content courses={parts} />
+        <Total courses={parts} />
       </>
     )}
 
